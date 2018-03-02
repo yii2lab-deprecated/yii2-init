b49e9e332f94c04c1fcbbbd91ac37c6b3cd55db7
Vitaliy <theyamshikov@yandex.ru>
2018-03-02 23:55:00 +0600

    refacto

--- src/domain/helpers/CheckYiiRequirements.php
+++ src/domain/helpers/CheckYiiRequirements.php
@@ -3,6 +3,7 @@
 namespace yii2lab\init\domain\helpers;
 
 use Imagick;
+use yii\helpers\ArrayHelper;
 use YiiRequirementChecker;
 
 class CheckYiiRequirements {
@@ -36,35 +37,41 @@ class CheckYiiRequirements {
 	
 	private static function checkMisc()
 	{
+		$requirementsChecker = self::createInstance();
+		$requirements = self::getAllRequirements($requirementsChecker);
+		$requirementsChecker->checkYii();
+		$requirementsChecker->check($requirements);
+		return $requirementsChecker;
+	}
+	
+	private static function getAllRequirements($requirementsChecker) {
+		$requirements = [];
+		$requirements = ArrayHelper::merge($requirements, self::getDatabaseRequirements());
+		$requirements = ArrayHelper::merge($requirements, self::getCacheRequirements());
+		$requirements = ArrayHelper::merge($requirements, self::getCaptchaRequirements());
+		$requirements = ArrayHelper::merge($requirements, self::getPhpIniRequirements($requirementsChecker));
+		$requirements = ArrayHelper::merge($requirements, self::getOpenSslRequirements());
+		$requirements = ArrayHelper::merge($requirements, self::getProjectRequirements());
+		return $requirements;
+	}
+	
+	private static function createInstance() {
 		require_once(VENDOR_DIR . SL . 'yiisoft/yii2/requirements/YiiRequirementChecker.php');
+		/** @var YiiRequirementChecker $requirementsChecker */
 		$requirementsChecker = new YiiRequirementChecker();
-		
-		$gdMemo = $imagickMemo = 'Either GD PHP extension with FreeType support or ImageMagick PHP extension with PNG support is required for image CAPTCHA.';
-		$gdOK = $imagickOK = false;
-		
-		if(extension_loaded('imagick')) {
-			$imagick = new Imagick();
-			$imagickFormats = $imagick->queryFormats('PNG');
-			if(in_array('PNG', $imagickFormats)) {
-				$imagickOK = true;
-			} else {
-				$imagickMemo = 'Imagick extension should be installed with PNG support in order to be used for image CAPTCHA.';
-			}
+		return $requirementsChecker;
+	}
+	
+	private static function getProjectRequirements() {
+		$appRequirements = @include(ROOT_DIR . '/environments/config/requirements.php');
+		if(empty($appRequirements)) {
+			return [];
 		}
-		
-		if(extension_loaded('gd')) {
-			$gdInfo = gd_info();
-			if(!empty($gdInfo['FreeType Support'])) {
-				$gdOK = true;
-			} else {
-				$gdMemo = 'GD extension should be installed with FreeType support in order to be used for image CAPTCHA.';
-			}
-		}
-		
-		/**
-		 * Adjust requirements according to your application specifics.
-		 */
-		$requirements = [
+		return $appRequirements;
+	}
+	
+	private static function getDatabaseRequirements() {
+		return [
 			// Database :
 			[
 				'name' => 'PDO extension',
@@ -93,6 +100,11 @@ class CheckYiiRequirements {
 				'by' => 'All DB-related classes',
 				'memo' => 'Required for PostgreSQL database.',
 			],
+		];
+	}
+	
+	private static function getCacheRequirements() {
+		return [
 			// Cache :
 			[
 				'name' => 'Memcache extension',
@@ -107,21 +119,11 @@ class CheckYiiRequirements {
 				'condition' => extension_loaded('apc'),
 				'by' => '<a href="http://www.yiiframework.com/doc-2.0/yii-caching-apccache.html">ApcCache</a>',
 			],
-			// CAPTCHA:
-			[
-				'name' => 'GD PHP extension with FreeType support',
-				'mandatory' => false,
-				'condition' => $gdOK,
-				'by' => '<a href="http://www.yiiframework.com/doc-2.0/yii-captcha-captcha.html">Captcha</a>',
-				'memo' => $gdMemo,
-			],
-			[
-				'name' => 'ImageMagick PHP extension with PNG support',
-				'mandatory' => false,
-				'condition' => $imagickOK,
-				'by' => '<a href="http://www.yiiframework.com/doc-2.0/yii-captcha-captcha.html">Captcha</a>',
-				'memo' => $imagickMemo,
-			],
+		];
+	}
+	
+	private static function getPhpIniRequirements(YiiRequirementChecker $requirementsChecker) {
+		return [
 			// PHP ini :
 			'phpExposePhp' => [
 				'name' => 'Expose PHP',
@@ -144,18 +146,65 @@ class CheckYiiRequirements {
 				'by' => 'Email sending',
 				'memo' => 'PHP mail SMTP server required',
 			],
-			
+		];
+	}
+	
+	private static function getOpenSslRequirements() {
+		return [
 			'openSsl' => [
 				'name' => 'OpenSSL',
 				'mandatory' => false,
 				'condition' => extension_loaded('openssl'),
-				//'by' => 'Email sending',
+				'by' => 'Generation cookie validation key',
 				'memo' => 'The OpenSSL PHP extension is required by Yii2.',
 			],
 		];
-		$requirementsChecker
-			->checkYii()
-			->check($requirements);
-		return $requirementsChecker;
 	}
+	
+	private static function getCaptchaRequirements() {
+		$gdMemo = $imagickMemo = 'Either GD PHP extension with FreeType support or ImageMagick PHP extension with PNG support is required for image CAPTCHA.';
+		$gdOK = $imagickOK = false;
+		
+		if(extension_loaded('imagick')) {
+			$imagick = new Imagick();
+			$imagickFormats = $imagick->queryFormats('PNG');
+			if(in_array('PNG', $imagickFormats)) {
+				$imagickOK = true;
+			} else {
+				$imagickMemo = 'Imagick extension should be installed with PNG support in order to be used for image CAPTCHA.';
+			}
+		}
+		
+		if(extension_loaded('gd')) {
+			$gdInfo = gd_info();
+			if(!empty($gdInfo['FreeType Support'])) {
+				$gdOK = true;
+			} else {
+				$gdMemo = 'GD extension should be installed with FreeType support in order to be used for image CAPTCHA.';
+			}
+		}
+		
+		/**
+		 * Adjust requirements according to your application specifics.
+		 */
+		$requirements = [
+			// CAPTCHA:
+			[
+				'name' => 'GD PHP extension with FreeType support',
+				'mandatory' => false,
+				'condition' => $gdOK,
+				'by' => '<a href="http://www.yiiframework.com/doc-2.0/yii-captcha-captcha.html">Captcha</a>',
+				'memo' => $gdMemo,
+			],
+			[
+				'name' => 'ImageMagick PHP extension with PNG support',
+				'mandatory' => false,
+				'condition' => $imagickOK,
+				'by' => '<a href="http://www.yiiframework.com/doc-2.0/yii-captcha-captcha.html">Captcha</a>',
+				'memo' => $imagickMemo,
+			],
+		];
+		return $requirements;
+	}
+	
 }
