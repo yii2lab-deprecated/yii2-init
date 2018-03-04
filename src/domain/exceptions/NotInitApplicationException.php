1fc00a8c2e2daa67e8a77d05522f6eac17dd0844
Vitaliy <theyamshikov@yandex.ru>
2018-03-04 18:44:10 +0600

    fix

--- src/domain/exceptions/NotInitApplicationException.php
+++ src/domain/exceptions/NotInitApplicationException.php
@@ -8,7 +8,6 @@ use yii\web\HttpException;
 class NotInitApplicationException extends HttpException {
 	
 	public function __construct($status = 500, $message = null, $code = 0, \Exception $previous = null) {
-		$message = 'Application not be inited!';
 		parent::__construct($status, $message, $code, $previous);
 	}
 	
