d54c04c2c4c27cf42e8e1e64c670d91df76a9946
Vitaliy <theyamshikov@yandex.ru>
2018-08-11 22:18:36 +0600

    upgrade rbac

--- src/admin/Module.php
+++ src/admin/Module.php
@@ -2,11 +2,23 @@
 
 namespace yii2lab\init\admin;
 
+use common\enums\rbac\PermissionEnum;
+use yii2lab\app\domain\enums\AppPermissionEnum;
+use yii2lab\applicationTemplate\common\enums\ApplicationPermissionEnum;
+use yii2lab\helpers\Behavior;
+
 /**
  * Class Module
  * 
  * @package yii2lab\init\admin
  */
 class Module extends \yii\base\Module {
+
+    public function behaviors()
+    {
+        return [
+            'access' => Behavior::access(ApplicationPermissionEnum::BACKEND_ALL),
+        ];
+    }
 
 }
